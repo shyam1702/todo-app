2a686b3320f4243c796056d20b2a9803d252db6e
Shyam <shyamhaldurai@gmail.com>
2025-06-08 14:06:27 +0530

    initial commit:  db connection setup

---
+++ backend/database.py
@@ -0,0 +1,16 @@
+# database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# connecting to the todo.db file in the same folder
+
+
+# Create the engine to interact with the database
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Create a session factory — we’ll use this to interact with the DB
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for models
+Base = declarative_base()
